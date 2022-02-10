2c65cb630d4522d3c627c24008d6839fab052c3f
Troy Eutermoser <troyhayeu@gmail.com>
2022-02-09 19:49:37 -0500

    added click event to start button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var renderedQuestion = document.getElementById("render-question")
+var renderedQuestion = document.getElementById("render-question");
 
-var renderedChoices = document.getElementById("choices")
+var renderedChoices = document.getElementById("choices");
 
 var startButton = document.querySelector(".start-button");
 
@@ -9,21 +9,22 @@ var questionsElement = document.querySelector(".questions");
 var timerElement = document.querySelector(".timer-count");
 
 var questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice: ["<scripting>", "<js>", "<javascript>", "<script>"],
-        answer: "<script>"
-    },
-    {
-        question: "How do you write \"Hello World\" in an alert box?",
-        choice: ["alertBox(Hello World);", "alert(Hello World);", "msgBox(Hello World);", "msg(Hello World);"],
-        answer: "alert(Hello World);"
-    },
-    
-]
-
-
-
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice: ["<scripting>", "<js>", "<javascript>", "<script>"],
+    answer: "<script>",
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    choice: [
+      "alertBox(Hello World);",
+      "alert(Hello World);",
+      "msgBox(Hello World);",
+      "msg(Hello World);",
+    ],
+    answer: "alert(Hello World);",
+  },
+];
 
 var questionsIndex = 0;
 var correctAnswers = 0;
@@ -33,17 +34,16 @@ var timerCount;
 var isWin = false;
 
 function renderQuestions() {
-    timerCount = 10
-renderedQuestion.innerHTML = questions[questionsIndex].question;
-var choices = questions[questionsIndex].choice;
+  timerCount = 10;
+  renderedQuestion.innerHTML = questions[questionsIndex].question;
+  var choices = questions[questionsIndex].choice;
 
-for (var i = 0; i < choices.length; i++) {
-  var selected = document.createElement("button");
-  selected.textContent = choices[i];
-  renderedChoices.appendChild(selected);
-  console.log("hello answer");
-}
-    
+  for (var i = 0; i < choices.length; i++) {
+    var selected = document.createElement("button");
+    selected.textContent = choices[i];
+    renderedChoices.appendChild(selected);
+    console.log("hello answer");
+  }
 }
 // function nextQuestion
 // increase questionsIndex++
@@ -51,32 +51,28 @@ for (var i = 0; i < choices.length; i++) {
 // call renderQuestions
 
 function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-        //   winGame();
-        }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
+  // Sets timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
         clearInterval(timer);
-        // loseGame();
+        //   winGame();
       }
-    }, 1000);
-  }
-
-
-renderQuestions()
-startTimer()
-var favoriteEl = document.createElement("div");
-// Create ordered list element
-var listEl = document.createElement("ol");
-// Create ordered list items
-var li1 = document.createElement("li");
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      // loseGame();
+    }
+  }, 1000);
+}
+function startQuiz() {
+  renderQuestions();
+  startTimer();
+}
+startButton.addEventListener("click", startQuiz);
